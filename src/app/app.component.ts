3e5485e1930de0f85e67fb9a8eaa4652feba62f6
Juan José Rendon <juan.rendon@yuxiglobal.com>
2017-11-21 22:42:02 -0500

    feat: initial aproach to AR using threejs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, animate } from '@angular/core';
+import { Scene , PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from 'three';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  scene = new Scene();
+  camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  renderer = new WebGLRenderer();
+  geometry =  new BoxGeometry(1, 1, 1);
+  material = new MeshBasicMaterial({ color: 0x00ff00  });
+  cube = new Mesh( this.geometry, this.material );
+
+  constructor() {
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.renderer.domElement );
+    this.scene.add(this.cube);
+    this.camera.position.z = 5;
+    this.animate();
+  }
+
+  animate() {
+    requestAnimationFrame( animate );
+    this.cube.rotation.x += 0.1;
+    this.cube.rotation.y += 0.1;
+    this.renderer.render( this.scene, this.camera );
+  }
+
 
 }
